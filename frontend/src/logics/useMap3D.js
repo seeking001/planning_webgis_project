801aba80ea65faa1f6c87669d2f01fe1ff6cfbde
seeking001 <313185240@qq.com>
2026-07-10 20:31:14 +0800

    20260710解决cesium地形显示TOKEN问题

--- frontend/src/logics/useMap3D.js
+++ frontend/src/logics/useMap3D.js
@@ -68,6 +68,8 @@ export function useMap3D(cesiumContainer, TIANDITU_API_KEY, buildingColors, defa
     Cesium = await import('cesium');
     await import('cesium/Build/Cesium/Widgets/widgets.css');
 
+    Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_TOKEN;
+
     const terrainProvider = await Cesium.createWorldTerrainAsync({
       requestVertexNormals: true,
     });
